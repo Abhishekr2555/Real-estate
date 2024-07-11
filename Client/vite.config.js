c1153e697618f1f0080363fe82f18b74dedbc81f
Abhishekr2555 <148955093+Abhishekr2555@users.noreply.github.com>
2024-07-11 18:22:31 +0530

    change in vite

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -4,7 +4,6 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Client/",
   resolve: {
     alias: {
       "~": path.resolve(__dirname, "node_modules"),
